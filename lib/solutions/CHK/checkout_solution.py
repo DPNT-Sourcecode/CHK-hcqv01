834058a3ae228b5b17dc4062689491ab2f11a169
sbx_user1051 <sbx_user1051@169.254.57.29>
2024-06-09 15:05:55 +0000

    Sun Jun 09 15:05:55 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,23 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     individual_skus = [x.strip() for x in skus.split(',')]
-    print(individual_skus)
-    return 10
+
+    prices = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
+
+    total_price = 0
+    for sku in skus:
+        print(sku)
+        total_price += prices[sku]
+
+    return total_price
+
+
+
+
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,9 +3,11 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        assert checkout_solution.checkout("A,B,C,C") == 0
+        assert checkout_solution.checkout("A,B") == 80
+
 
     # def test_checkout_illegal(self):
     #     assert checkout_solution.checkout(123) == -1
+
 
 
